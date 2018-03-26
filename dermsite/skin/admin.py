12c3a4f4caf050a7ac93a78f4ae1bc81b254590d
VLaD0S <tudorvladneacsu@hotmail.co.uk>
2018-03-26 12:41:16 +0100

    Successfully added the skin app within the django project.

--- dermsite/skin/admin.py
+++ dermsite/skin/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Dig
 
+admin.site.register(Dig)
 # Register your models here.
--- dermsite/skin/models.py
+++ dermsite/skin/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+# diagnosis class
+class Dig(models.Model):
+
+    iden_key = models.CharField(max_length=256)
+    image_path = models.CharField(max_length=256)
+    top_prediction = models.FloatField()
+
+    def __str__(self):
+        return self.image_path
